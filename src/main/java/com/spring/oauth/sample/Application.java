f001b62b4e7d025c77b0c859cf3b0ec88b03aa7c
LeoChiu1027 <lunayccleo@gmail.com>
2020-07-08 18:03:09 +0800

    Update Application.java

--- src/main/java/com/spring/oauth/sample/Application.java
+++ src/main/java/com/spring/oauth/sample/Application.java
@@ -1,15 +1,10 @@
 package com.spring.oauth.sample;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
